3d37b994920fdff2f9b818a2907e5763d0ca9070
Essa <126317280+essa44857@users.noreply.github.com>
2026-07-23 08:19:35 +0300

    Remove comments from student.h

--- student.h
+++ student.h
@@ -15,23 +15,23 @@ typedef struct {
     int capacity;
 } StudentDatabase;
 
-// 1. Initialization and Memory Cleanup
+
 void init_db(StudentDatabase *db, int initial_capacity);
 void free_db(StudentDatabase *db);
 
-// 2. Addition and Display
+
 void add_student(StudentDatabase *db, Student s);
 void display_all(const StudentDatabase *db);
 
-// 3. Linear Search
+
 int search_by_id_linear(const StudentDatabase *db, int id);
 int search_by_salary_linear(const StudentDatabase *db, float salary);
 
-// 4. Sorting and Binary Search
+// 4. 
 void bubble_sort_by_id(StudentDatabase *db);
 int binary_search_by_id(const StudentDatabase *db, int id);
 
-// 5. Modification and Deletion (Using Index)
+
 void modify_student(StudentDatabase *db, int index);
 void delete_student(StudentDatabase *db, int index);
 
